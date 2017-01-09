4bab9a3645d33cbd43c4ce35c7d2e071fbd890ae
Saurav_Agarwal <saurav6@gmail.com>
2017-01-08 18:20:21 -0600

    added twod linear robot motion model

---
+++ MotionModels/TwoDPointRobot.m
@@ -0,0 +1,54 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Belief Space Planning with iLQG
+% Copyright 2017
+% Author: Saurav Agarwal
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% 2D Point robot with additive process noise.
+classdef TwoDPointRobot < MotionModelBase
+    properties (Constant = true) % note that all properties must be constant, because we have a lot of copies of this object and it can take a lot of memory otherwise.
+        stDim = 2; % state dimension
+        ctDim = 2;  % control vector dimension
+        wDim = 2;   % Process noise (W) dimension
+        P_Wg = diag([0.01,0.01].^2); % covariance of state-additive-noise
+        sigma_b_u = [0.0;0.0]; % A constant bias intensity (std dev) of the control noise
+        eta_u = [0;0]; % A coefficient, which makes the control noise intensity proportional to the control signal       
+        zeroNoise = [0;0];
+    end
+    
+    properties
+        dt = 0.1; % delta_t for time discretization
+    end
+    
+    methods
+        
+        function obj = TwoDPointRobot()
+            obj@MotionModelBase();                       
+        end
+        
+        function x_next = evolve(obj,x,u,w) % discrete motion model equation            
+            x_next = x + u*obj.dt + w*sqrt(obj.dt);
+        end
+        
+        function A = getStateTransitionJacobian(obj,x,u,w) % state Jacobian
+            A = eye(2);
+        end
+        
+        function B = getControlJacobian(obj,x,u,w) % control Jacobian
+            B = obj.dt*eye(2);
+        end
+        
+        function G = getProcessNoiseJacobian(obj,x,u,w) % noise Jacobian
+            G = sqrt(obj.dt)*eye(2);
+        end
+        
+        function Q = getProcessNoiseCovariance(obj,x,u) % compute the covariance of process noise based on the current poistion and controls
+            Q = obj.P_Wg;
+        end
+        
+        function w = generateProcessNoise(obj,x,u) % simulate (generate) process noise based on the current state and controls
+            w = mvnrnd(zeros(obj.stDim,1),obj.P_Wg)';
+        end
+        
+    end
+end
